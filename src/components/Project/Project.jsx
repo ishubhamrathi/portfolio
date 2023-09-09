c039f7f28a4e782741dd41f931836c70a00bfb2d
Shubham Rathi <shubh2q2q@gmail.com>
2023-09-10 00:34:57 +0530

    issues fixed with project section fixed

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Project.module.css'
-import {AiFillGithub, AiOutlineCode} from 'react-icons/ai'
+import {AiFillGithub, AiOutlineCode, AiFillInfoCircle} from 'react-icons/ai'
 import {BiLinkAlt} from 'react-icons/bi'
+import { current } from '@reduxjs/toolkit'
 
 const Project = (props) => {
-  const {title, img, github_link, deployed_link, tech}=props;
-  
+  const {title, description, img, github_link, deployed_link, tech}=props;
+  const [isActive, setIsActive]=useState(false);
+  const handleClick=()=>{
+    setIsActive(current=>!current);
+  }
+  useEffect(() => {
+    const savedIsActive = localStorage.getItem('isActive');
+    if (savedIsActive !== null) {
+      setIsActive(savedIsActive === 'true');
+    }
+  }, []);
+// window.onload({
+//   const [isActive, setIsActive]=useState(false);
+// })
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={handleClick}>
       <div className={styles.bgimg}><img src={img} alt=""/></div>
+      <div className={styles.title}>{title}</div> 
       <div className={styles.links}>
         <div className={styles.github}><a href={github_link}><AiFillGithub/>&nbsp;Github</a></div>
         <div className={styles.deploy}><a href={deployed_link}><BiLinkAlt/>&nbsp;View</a></div>
       </div>
-      <div className={styles.title}>{title}</div> 
-      <div className={styles.tech}>
-        <div className={styles.techwrapper}><AiOutlineCode/>:&nbsp;{tech.map((item)=> {return <span>{item}&nbsp;</span>})}</div>
+      <div className={isActive ? `${styles.info}` :`${styles.hidden}`}>
+        <div className={styles.desc}>
+         <AiFillInfoCircle/>&nbsp;Description:&nbsp;{description}
+        </div>
+        <div className={styles.tech}>
+         <AiOutlineCode/>:&nbsp;{tech.map((item)=> {return <span>{item}&nbsp;</span>})}
+        </div>
       </div>
+      
+      
     </div>
   )
 }
