9010a61c8d3e53c8c0681f59da0c274f6abf81bd
Shubham Rathi <shubh2q2q@gmail.com>
2023-09-09 10:03:30 +0530

    Porjects section improved

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import styles from './Project.module.css'
+import {AiFillGithub, AiOutlineCode} from 'react-icons/ai'
+import {BiLinkAlt} from 'react-icons/bi'
 
-const Project = () => {
+const Project = (props) => {
+  const {title, img, githublink, deployedlink, tech}=props;
   return (
-    <div>Project</div>
+    <div className={styles.container}>
+      <div className={styles.title}>{title}</div> 
+      <div className={styles.bgimg}><img src={img} alt=""/></div>
+      <div className={styles.links}> 
+        <div className={styles.github}><a href={githublink}><AiFillGithub/>&nbsp;Github</a></div>
+        
+        <div className={styles.deploy}><a href={deployedlink}><BiLinkAlt/>&nbsp;View</a></div>
+      </div>
+      <div className={styles.tech}> <AiOutlineCode/>:&nbsp;{tech.map((item)=> {return <span>{item}&nbsp;</span>})}
+      </div>
+
+    </div>
   )
 }
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import styles from './projects.module.css'
+import Project from '../Project/Project'
 const Projects = () => {
   return (
     <div className={styles.container}>
       <div className={styles.title}>Projects</div>
       <div className={styles.list}>
-        <div className={styles.cards}>aa</div>
-        <div className={styles.cards}>aa</div>
-        <div className={styles.cards}>aa</div>
+          <Project title="Music App" description="Lipsum Lorem"
+            img="https://i.ibb.co/mJ45n93/project1.png"
+            tech={["ReactJs"]} github_link=""
+            deployed_link=""
+          />
+
+          <Project title="Ums Web App" description="Lipsum Lorem"
+            img="https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+            tech={["ReactJs", "NodeJs"]} github_link=""
+            deployed_link=""
+          />
+
+          <Project title="Ums Web App" description="Lipsum Lorem"
+            img="https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+            tech={["ReactJs","sdsdsd","sdsdsd","sdsdsd","sdsdsd","sdsdsd","sdsdsd","sdsdsd"]} github_link=""
+            deployed_link=""
+          />
       </div>
     </div>
   )
